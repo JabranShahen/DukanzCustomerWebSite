db3fe6d74481de12806c23cb0fca11db56be4fda
Jabran Shaheen <34131257+JabranShahen@users.noreply.github.com>
2026-03-10 15:37:33 +0000

    SeedWebapp

---
+++ src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PrivacyPolicyPageComponent } from './pages/privacy-policy-page/privacy-policy-page.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+    title: 'Dukanz'
+  },
+  {
+    path: 'privacy-policy',
+    component: PrivacyPolicyPageComponent,
+    title: 'Dukanz | Privacy Policy'
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
---
+++ src/app/pages/privacy-policy-page/privacy-policy-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-privacy-policy-page',
+  imports: [RouterLink],
+  templateUrl: './privacy-policy-page.component.html',
+  styleUrl: './privacy-policy-page.component.css'
+})
+export class PrivacyPolicyPageComponent {}
